fe40f7c5572f7c8be9ff17f67e409635ef7429ef
Kaleb Coberly <kalebcoberly@gmail.com>
2025-05-28 15:14:19 -0700

    Skip GUI tests in act.

--- tests/basic-ui.test.js
+++ tests/basic-ui.test.js
@@ -2,21 +2,28 @@ const { test, expect } = require("@playwright/test");
 const path = require("path");
 const { _electron: electron } = require("playwright");
 
-test.describe("Electron basic app test", () => {
-  let app;
+let app;
+const skipElectronTests = process.env.DISABLE_ELECTRON_TESTS === "true";
 
-  test.beforeAll(async () => {
+test.describe("Electron basic app test", () => {
+  test.beforeAll(async ({}, testInfo) => {
+    if (skipElectronTests) {
+      test.skip(true, "Skipping Electron tests in CI/headless mode");
+    }
+
     app = await electron.launch({ args: ["."] });
   });
 
   test.afterAll(async () => {
-    await app.close();
+    if (app) {
+      await app.close();
+    }
   });
 
   test("should open a window and load the first page", async () => {
     const window = await app.firstWindow();
     const title = await window.title();
-    expect(title).toBe(""); // Set <title> in your HTML if needed
+    expect(title).toBe("");
   });
 
   test("should show the button to go next", async () => {
